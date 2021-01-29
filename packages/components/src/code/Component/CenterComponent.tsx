b2d4494cbb2809590bdf85428414722bf5340c2f
Rita <rita.chaple@codegrabs.com>
2021-01-29 16:03:49 +0530

    add checkbox

--- packages/components/src/code/Component/CenterComponent.tsx
+++ packages/components/src/code/Component/CenterComponent.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const CenterComponent = () => {
-  return <View />
+  return (
+    <View>
+      <Text style={[styles.appButtonText]}>Select All</Text>
+      <Icon
+        style={{ alignSelf: 'center', paddingHorizontal: 2 }}
+        name="square-o"
+        size={10}
+        color="#000"
+      />
+    </View>
+  )
 }
 
 export default CenterComponent
+
+const styles = StyleSheet.create({
+  appButtonText: {
+    fontSize: 10,
+    alignSelf: 'center',
+    color: '#000',
+  },
+})
--- packages/components/src/code/Component/RightComponent.tsx
+++ packages/components/src/code/Component/RightComponent.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import { Button } from 'react-native-elements'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 const OutlineButton = (props: any) => {
