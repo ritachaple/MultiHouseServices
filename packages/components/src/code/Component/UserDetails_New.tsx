6ae13824f80d69a2641d0be5fc8f5180856d322f
Mayur Codegrabs <mayur.malewar@gmail.com>
2021-02-16 19:41:17 +0530

    update

--- packages/components/src/code/Component/UserDetails_New.tsx
+++ packages/components/src/code/Component/UserDetails_New.tsx
@@ -7,20 +7,100 @@ import {
   ScrollView,
   TouchableOpacity,
 } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const UserData = () => {
   return (
     <View
       style={{
-        // backgroundColor:"steelblue",
         flex: 1,
-        padding: '1%',
+        // backgroundColor:"steelblue",
+        // padding: '1%',
+        paddingVertical: '5%',
+        paddingHorizontal: '10%',
+        borderWidth: 1,
+        borderRadius: 10,
+        margin: 20,
       }}
     >
-      <View>
-        <InputField label="" placeholder="" />
-        <InputField label="" placeholder="" />
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Text>User Details</Text>
+        <Icon name="window-close" size={20} />
       </View>
+      <View
+        style={{
+          flex: 5,
+          // justifyContent:"center",
+          // alignItems:"center"
+          // backgroundColor:"green"
+        }}
+      >
+        {/* <TextInput placeholder="hello" style={{backgroundColor:'white', }}></TextInput> */}
+        <View
+          style={{
+            flexDirection: 'row',
+            flex: 1,
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <InputField label="First Name" placeholder="First Name" />
+          <InputField label="Last Name" placeholder="Last Name" />
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            flex: 1,
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <InputField label="Email" placeholder="Email" />
+          <InputField label="Phone Number" placeholder="Phone Number" />
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            flex: 1,
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <InputField label="Location" placeholder="Location" />
+          <InputField label="Whatsapp Number" placeholder="Whatsapp Number" />
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            flex: 1,
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <InputField label="Facebook ID" placeholder="Facebook ID" />
+          <InputField label="Twitter ID" placeholder="Twitter ID" />
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            flex: 1,
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <InputField label="Instagram ID" placeholder="Instagram ID" />
+          <InputField label="Policy Number" placeholder="Policy Number" />
+        </View>
+        {/* <View style={{flexDirection:'row',flex:1 ,justifyContent: 'space-between', alignItems:"center" }}>
+            </View> */}
+      </View>
+      <View style={{ flex: 1 }} />
     </View>
   )
 }
@@ -30,14 +110,14 @@ export default UserData
 export const InputField = (props: any) => {
   const { label, placeholder } = props
   return (
-    <View>
+    <View style={{ flex: 1, padding: '3%' }}>
       <Text>{label}</Text>
       <TextInput
         style={{
           borderWidth: 1,
           borderRadius: 3,
-          padding: '0.5%',
-          width: '30%',
+          padding: '2%',
+          width: '100%',
           // backgroundColor:
         }}
         placeholder={placeholder}
